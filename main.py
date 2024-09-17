82d12a2aaf1548adfbee04af4214f42479b81a42
Victormarsh001 <victorbasy17.3@gmail.com>
2024-09-17 20:29:47 +0000

    Commit2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World" #render_template("home.html")
+  return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
