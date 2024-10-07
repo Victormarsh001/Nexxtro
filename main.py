03e905730ba2fee56a31bf756cb4213b7de183d7
Victormarsh001 <victorbasy17.3@gmail.com>
2024-10-07 11:41:26 +0000

    Commit 11
    Community Discussion, make post, comment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 from flask import Flask,request, render_template, session, redirect, url_for
 import sqlite3
+from datetime import datetime
 from os import environ
 from secrets import token_urlsafe
 import smtplib
@@ -33,9 +34,9 @@ posts.execute("CREATE TABLE IF NOT EXISTS posts(id Text, email Text, name Text, 
 
 posts.execute("CREATE TABLE IF NOT EXISTS comments(id Text, name Text, date Text, comment Text)")
 
-posts.execute("Insert into comments Values(:id, :name, :date, :comment)", {'id':'1', 'name':'Victor Basy', 'date':'6/10/2021', 'comment':'You too man'})
+#posts.execute("Insert into comments Values(:id, :name, :date, :comment)", {'id':'1', 'name':'Victor Basy', 'date':'6/10/2021', 'comment':'You too man'})
 
-posts.execute("Insert into posts Values(:id, :email, :name, :date, :post)", {'id':'1', 'email':'victorbasy17.3@gmail.com', 'name':'Victor Basy', 'date':'6/10/2021', 'post':'Have a nice trading day everyone!!'})
+#posts.execute("Insert into posts Values(:id, :email, :name, :date, :post)", {'id':'1', 'email':'victorbasy17.3@gmail.com', 'name':'Victor Basy', 'date':'6/10/2021', 'post':'Have a nice trading day everyone!!'})
 co.commit()
 co.close()
 
@@ -66,22 +67,58 @@ if not app.secret_key:
 
 @app.route("/")
 def hello():
-  return redirect(url_for("post"))
+  return redirect(url_for("login"))
+
+@app.route("/publisher", methods=["GET", "POST"])
+def publisher():
+  if request.method == "POST":
+    post = request.form.get("post")
+    date = datetime.now().strftime("%d/%m/%Y")
+    
+    conn = getPost()
+    cursor = conn.cursor()
+    cursor.execute("Select id from posts")
+    id = len(cursor.fetchall()) + 1
+    cursor.execute("Insert into posts Values(:id, :email, :name, :date, :post)", {'id':str(id), 'email':session['email'], 'name':session['name'], 'date':date, 'post':post})
+    conn.commit()
+    conn.close()
+    return redirect(url_for("post"))
+                                  
+  return render_template("publisher.html")
+
+@app.route("/comment", methods=["GET","POST"])
+def comment():
+  if request.method == "POST":
+    id = session['commentId']
+    comment = request.form.get("comments")
+    date = datetime.now().strftime("%d/%m/%Y")
+    name = session['name']
+
+    conn = getPost()
+    cursor = conn.cursor()
+    cursor.execute("Insert into comments Values(:id, :name, :date, :comment)",{'id':id, 'name':name, 'date':date, 'comment':comment})
+    conn.commit()
+    conn.close()
+    return redirect(url_for("commentView"))
+  
+  return render_template("comment.html")
 
 @app.route("/commentView")
 def commentView():
   id = request.args.get("id")
   name = request.args.get("name")
-  if not id or not name:
-    return redirect(url_for("post"))
+
+  if 'commentId' not in session or 'name'  not in session:
+    session['commentId'] = id
+    session['name'] = name
+
   
   con = getPost()
   cursor = con.cursor()
-  cursor.execute("Select comment, name, date from comments where id = :id", {'id':id})
-  data = cursor.fetchall()
+  cursor.execute("Select * from comments where id = :id", {'id':session['commentId']})
+  data = cursor.fetchall()[::-1]
   con.close()
-  
-  return render_template("commentView.html", data=data, name=name)
+  return render_template("commentView.html", data=data, name=session['name'])
 
 
 @app.route("/post")
@@ -89,7 +126,7 @@ def post():
   post = getPost()
   cursor = post.cursor()
   cursor.execute("Select * from posts")
-  data = cursor.fetchall()
+  data = cursor.fetchall()[::-1]
   post.close()
   return render_template("post.html", data=data)
 
@@ -125,10 +162,11 @@ def loginDetail():
     password = request.form["password"]
     con = getConn()
     cursor = con.cursor()
-    cursor.execute("SELECT password from users Where email = :email", {"email": email})
+    cursor.execute("SELECT password, name from users Where email = :email", {"email": email})
     data = cursor.fetchone()
     print(data)
     if data and data[0] == password:
+      session['name'] = data[1]
       con.close()
       session['email'] = email
       return redirect(url_for("account"))
